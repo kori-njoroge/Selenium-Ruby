ee33ad5c424e7a3e71c05dffeebda1f29e9f42ee
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-22 13:46:01 +0300

    && <AND> in Ruby

--- conditionalexecution.rb
+++ conditionalexecution.rb
@@ -17,3 +17,28 @@ elsif x > 0
 else
     puts "Zero!"
 end
+
+# Condtional expressions
+# && - AND- Both condtions need to be true
+# || - OR- One condition need to be true
+# != - Not equal to
+# == equal to
+
+
+# AND
+
+grade = 2.5
+
+if grade == 4.0
+    puts "You are an awesome student"
+elsif grade < 4.0 && grade >=3.5
+    put "A Student"
+elsif grade <3.5 && grade >= 3.0
+    puts "B student"
+elsif grade < 3.0 && grade >=2.0
+    puts " C student"
+elsif grade < 2.0 && grade >=1.5
+    puts "D Student"
+else
+    puts "You failed the class"
+end
