d9551b7184a3e1579fe184a6289f4bf1ad9c687c
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-22 14:16:30 +0300

    Switch case

---
+++ switch.rb
@@ -0,0 +1,18 @@
+# SWITCH CASE 
+grade = 'A'
+
+case (grade.upcase)
+when 'A+','A','A-'
+    puts "Great Student"
+when 'B+','B','B-'
+    puts "Good Student"
+when 'C+','C','C-'
+    puts 'Poor student'
+when 'D+','D','D-'
+    puts "Very Poor Student"
+else
+    puts 'Failed'
+end
+
+
+
