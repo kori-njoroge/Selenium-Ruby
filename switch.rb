e6d784e115350ceaf8b415f2726b9c0d44ac6051
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-22 14:16:54 +0300

    switch case using  '..'

--- switch.rb
+++ switch.rb
@@ -15,4 +15,17 @@ else
 end
 
 
+# USING ..
 
+credit_score = 750
+
+case (credit_score)
+when  750..850
+    puts "You have exellent credit"
+when 650..749
+    puts "You are approved for loan insterest of 4.0"
+when 500..649
+    puts "You are approved for a loan interest of 6.0"
+else 
+    puts "You are not pre-approved sorry"
+end
