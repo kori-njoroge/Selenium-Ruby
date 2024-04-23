f61bf71dbbc7384af8f3790e1a2ba5c495998b97
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-23 14:42:56 +0300

    Second question

--- looping_assignment.rb
+++ looping_assignment.rb
@@ -1,1 +1,11 @@
+# question 1 - Create a simple for loop in Ruby that prints "Hello World" 10 times.
 10.times{puts "Hello world"}
+
+# Question2 "city_array = ['New York', 'San Francisco', 'Dallas', 'Los Angeles', 'Seattle']
+# Using for loop, print out each element of city_array."
+
+city_array = ['New York', 'San Francisco', 'Dallas', 'Los Angeles', 'Seattle']
+
+for city in city_array
+    puts "City: #{city}"
+end
