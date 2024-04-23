ba645c1b67afb9c39b5024e13019aed976278d17
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-23 13:52:07 +0300

    Arrays

---
+++ arrays.rb
@@ -0,0 +1,21 @@
+# student_names = ['John',"Joseph",'Jacob','Mike','Ross']
+
+# puts student_names[0]
+
+# creating a new array
+student_array = Array.new
+puts student_array
+
+# fill array with elements
+student_array[0] = "Jane"
+student_array[1] = "Doe"
+student_array[2] = "Mike"
+student_array[3] = "Ross"
+
+puts student_array #prints the elements of the array
+
+print student_array #prints the array with the elements
+print "\n"
+puts student_array.length
+puts student_array.empty?
+print student_array.sort
