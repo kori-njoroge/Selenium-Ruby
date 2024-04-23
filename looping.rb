9ed94f1f6678e73b890f8465f0efcb0406eb0b2f
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-23 14:21:25 +0300

    More examples on for loop

--- looping.rb
+++ looping.rb
@@ -9,15 +9,30 @@
 # end
 
 #  find a number greater than 2
-for i in 1..10
-    if i > 2 then 
-        puts "The value of i is #{i}"
-    end
-end
+# for i in 1..10
+#     if i > 2 then 
+#         puts "The value of i is #{i}"
+#     end
+# end
 
 # find even numbers in 0..10
 for i in 0..10
     if i%2 == 0 then
         puts "#{i} is an even number"
+    else
+        puts "#{i} is an odd number"
+    end
+end
+
+# for loop to iterate over an array
+city_array = ['New York','New Jersey','Chicago','Los Angeles','Arizona']
+
+for city in city_array
+    puts "City: #{city}"
+end
+
+for i in city_array
+    if i.capitalize == "Arizona"
+        puts "Welcome to my city City: #{city}"
     end
 end
