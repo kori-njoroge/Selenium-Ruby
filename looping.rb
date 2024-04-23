92286ae83586b53cce1a8a9d6fafd8dc4658f9bf
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-23 14:38:03 +0300

    each loop in ruby

--- looping.rb
+++ looping.rb
@@ -4,59 +4,69 @@
 # printing "hello world 10 times"
 
 # For loop
-# for i in 1..10
-#     puts "Hello world #{i}"
-# end
+for i in 1..10
+    puts "Hello world #{i}"
+end
 
 #  find a number greater than 2
-# for i in 1..10
-#     if i > 2 then 
-#         puts "The value of i is #{i}"
-#     end
-# end
+for i in 1..10
+    if i > 2 then 
+        puts "The value of i is #{i}"
+    end
+end
 
 # find even numbers in 0..10
-# for i in 0..10
-#     if i%2 == 0 then
-#         puts "#{i} is an even number"
-#     else
-#         puts "#{i} is an odd number"
-#     end
-# end
+for i in 0..10
+    if i%2 == 0 then
+        puts "#{i} is an even number"
+    else
+        puts "#{i} is an odd number"
+    end
+end
 
-# # for loop to iterate over an array
-# city_array = ['New York','New Jersey','Chicago','Los Angeles','Arizona']
+# for loop to iterate over an array
+city_array = ['New York','New Jersey','Chicago','Los Angeles','Arizona']
 
-# for city in city_array
-#     puts "City: #{city}"
-# end
+for city in city_array
+    puts "City: #{city}"
+end
 
-# for i in city_array
-#     if i.capitalize == "Arizona"
-#         puts "Welcome to my city City: #{city}"
-#     end
-# end
+for i in city_array
+    if i.capitalize == "Arizona"
+        puts "Welcome to my city City: #{city}"
+    end
+end
 
-# # WHILE LOOP - executes untill a condtion is true
-# x = 10
-# number  = 4
+# WHILE LOOP - executes untill a condtion is true
+x = 10
+number  = 4
 
-# while x>= number
-#     puts "The number is #{x}"
-#     x-=1
-# end
+while x>= number
+    puts "The number is #{x}"
+    x-=1
+end
 
-# # UNTIL LOOP - opposite of while loop
-# x = 8
-# max_number = 10
+# UNTIL LOOP - opposite of while loop
+x = 8
+max_number = 10
 
-# until x >= max_number
-#     puts "Number is #{x}"
-#     x+=1
-# end
+until x >= max_number
+    puts "Number is #{x}"
+    x+=1
+end
 
 
 # ITERATION
 # times
-
 10.times{ puts "Hello World"}
+
+# EACH LOOP - Used primarily in hashes and arrays
+counties_array = ['polk','fairfox','orange']
+
+# for county in counties_array
+#     puts "county: #{county}"
+# end
+
+counties_array.each do |county|
+    puts "county: #{county}"
+end
