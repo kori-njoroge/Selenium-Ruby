e9ebdbed2036766378bb19835aae311609119110
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-23 14:14:34 +0300

    For loop

---
+++ looping.rb
@@ -0,0 +1,9 @@
+# Loopig - seaquence of instructions that is continually repeated until a certain condition is reached
+# looping is used to iterate over the range of number, text array elements, hash elements and so on.
+
+# printing "hello world 10 times"
+
+# For loop
+for i in 1..10
+    puts "Hello world #{i}"
+end
