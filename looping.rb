986567cdf845620fcc49f876739488ec186f6b0c
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-23 14:14:56 +0300

    for loop applications

--- looping.rb
+++ looping.rb
@@ -4,6 +4,20 @@
 # printing "hello world 10 times"
 
 # For loop
+# for i in 1..10
+#     puts "Hello world #{i}"
+# end
+
+#  find a number greater than 2
 for i in 1..10
-    puts "Hello world #{i}"
+    if i > 2 then 
+        puts "The value of i is #{i}"
+    end
+end
+
+# find even numbers in 0..10
+for i in 0..10
+    if i%2 == 0 then
+        puts "#{i} is an even number"
+    end
 end
