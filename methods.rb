09e3df33f419a24fb0cf8a95b5d62ef2e53b647c
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-25 16:26:00 +0300

    Method to add 2 numbers

---
+++ methods.rb
@@ -0,0 +1,8 @@
+# ruby Methods are used to bundle one or more  repeatable statements into a single unit
+
+# Method to add 2 numbers
+def add(num1, num2)
+    return num1 + num2
+end
+
+puts add(100,1)
