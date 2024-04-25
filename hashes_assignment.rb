659e023986b20bcf34327ebcb0ace2bb5475393d
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-25 15:58:44 +0300

    hashes_assignment.rb

--- hashes_assignment.rb
+++ hashes_assignment.rb
@@ -9,3 +9,10 @@ celebrity_hash = {'Beyonce' => 252424, 'Michael Jackson' => 4243433, 'Sam Smith'
 print celebrity_hash.values
 puts " "
 
+# Using each loop, print out each element of the celebrity_hash.
+celebrity_hash = {'Beyonce' => 252424, 'Michael Jackson' => 4243433, 'Sam Smith' => 52134434, 'Taylor Swift' => 7534343, 'Tommy Smith' => 523232}
+
+celebrity_hash.each{|element|
+    puts "#{element}"
+}
+
