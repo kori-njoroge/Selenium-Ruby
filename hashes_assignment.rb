0b8090f7b248c3196dd90d0473377f2d51b197c5
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-25 15:58:05 +0300

    n 2

--- hashes_assignment.rb
+++ hashes_assignment.rb
@@ -3,3 +3,9 @@ celebrity_hash = {'Beyonce' => 252424, 'Michael Jackson' => 4243433, 'Sam Smith'
 
 print celebrity_hash.keys
 puts " "
+# Get all the values from the celebrity_hash.
+celebrity_hash = {'Beyonce' => 252424, 'Michael Jackson' => 4243433, 'Sam Smith' => 52134434, 'Taylor Swift' => 7534343, 'Tommy Smith' => 523232}
+
+print celebrity_hash.values
+puts " "
+
