a92f6d9513ba978d22b0df9cfe6b35b2e5bc3ee8
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-25 15:57:28 +0300

    Qn 1

---
+++ hashes_assignment.rb
@@ -0,0 +1,5 @@
+# Get all the keys from the celebrity_hash. 
+celebrity_hash = {'Beyonce' => 252424, 'Michael Jackson' => 4243433, 'Sam Smith' => 52134434, 'Taylor Swift' => 7534343, 'Tommy Smith' => 523232}
+
+print celebrity_hash.keys
+puts " "
