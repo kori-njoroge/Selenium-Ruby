309ae2c4c9c2780ce29abd55cfd27d30c189d47d
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-27 22:23:55 +0300

    ruby intellisense set up

--- classes.rb
+++ classes.rb
@@ -1,20 +1,21 @@
 # class - Specification or a blue print of how to provide some service
 
-
 # creating a class
 class Dog
-    def name_of_dog(name)
-        puts "I am #{name}"
-    end
-    def sleeping
-        puts "The dog is sleeping"
-    end
-    def playing
-        puts "lets play"
-    end
+  def name_of_dog(name)
+    puts "I am #{name}"
+  end
+
+  def sleeping
+    puts 'The dog is sleeping'
+  end
+
+  def playing
+    puts 'lets play'
+  end
 end
 
 # create an object from the class
 bosco = Dog.new
 
-bosco.name_of_dog("Bosco")
+bosco.name_of_dog('Bosco')
