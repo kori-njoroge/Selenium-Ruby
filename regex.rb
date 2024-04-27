1a760022d2381d192ab91cce2a35584a76ba103e
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-27 23:49:32 +0300

    match the beginning of a string

--- regex.rb
+++ regex.rb
@@ -7,7 +7,7 @@
 
 word1 = "Hello Man"
 word2 ="Hello"
-word3 = "matchesString"
+word3 = "matches String"
 
 def match_with_word(string1, string2)
     if string1 =~ /#{string2}/ #finding string 2 in string 1
@@ -22,3 +22,12 @@ match_with_word(word1, word2.capitalize)
 match_with_word(word1, word3.capitalize)
 
 
+def match_begining_word(string1, regex_pattern)
+    if string1 =~ /#{regex_pattern}/
+        puts "#{string1} begins with: '#{regex_pattern}'"
+    else
+        puts "No match"
+    end
+end
+
+match_begining_word(word3, "matches")
