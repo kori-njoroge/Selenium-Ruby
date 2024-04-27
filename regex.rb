1f61a56cf00784d6e6c8b5043b7398cc8f3d6fe2
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-28 00:00:50 +0300

    starts with and ends with

--- regex.rb
+++ regex.rb
@@ -7,7 +7,7 @@
 
 word1 = "Hello Man"
 word2 ="Hello"
-word3 = "matches String"
+word3 = "matches string"
 
 def match_with_word(string1, string2)
     if string1 =~ /#{string2}/ #finding string 2 in string 1
@@ -23,7 +23,7 @@ match_with_word(word1, word3.capitalize)
 
 
 def match_begining_word(string1, regex_pattern)
-    if string1 =~ /#{regex_pattern}/
+    if string1 =~ /^#{regex_pattern}$/
         puts "#{string1} begins with: '#{regex_pattern}'"
     else
         puts "No match"
@@ -31,3 +31,27 @@ def match_begining_word(string1, regex_pattern)
 end
 
 match_begining_word(word3, "matches")
+
+def string_ends_with(string1, regex_pattern)
+    if string1 =~ /#{regex_pattern}/
+        puts "'#{string1}' ends with: '#{regex_pattern}'"
+    else
+        puts "No match"
+    end
+end
+
+
+string_ends_with(word3,"string")
+
+# Starts with and Ends with
+def starts_with_and_ends_with(string1, reg_start, reg_end)
+    # if string1 =~ /^#{reg_start}/  && string1 =~ /#{reg_end}$/
+    if string1 =~ /^#{reg_start}.*#{reg_end}$/
+        puts "'#{string1}' starts with '#{reg_start}' ends with: '#{reg_end}'"
+    else
+        puts "No Match"
+    end
+end
+
+
+starts_with_and_ends_with(word3, "matches","string")
