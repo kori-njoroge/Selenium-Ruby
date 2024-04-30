99917f6860fe25e0d83ce6b55be24725d539835f
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-30 11:18:35 +0300

    Reading the contents of a file

--- files.rb
+++ files.rb
@@ -4,3 +4,8 @@ puts File.exist?('arrays.rb')
 # write contents in a file
 aFile = File.new("Ruby.txt",'w')
 aFile.syswrite("I have mastered the basics of Ruby now")
+aFile.close
+
+# Reading the conntent of the file
+aFile2 = File.open("Ruby.txt","r")
+aFile2.each{|line| puts line} 
