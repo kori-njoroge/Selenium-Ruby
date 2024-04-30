b1f8e0907df42c5e02b922ef02d7a834024cc110
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-30 08:44:24 +0300

    Encapsulation and example 1

---
+++ oop.rb
@@ -0,0 +1,26 @@
+# Model organized around objects rather than actions and data rather than login
+
+# OOP TYPES
+# 1. ENCAPSULATION - packing of data and functions into a single components
+#   => Can be used to prevent modification of variables from objects
+# Atttribute Accessory types
+#  => Read only Access
+#  => Write Access
+#  => Read & Write Access.
+
+class Document
+    attr_accessor :name
+    def initialize(doc_name)
+        @name = doc_name
+    end
+
+    def set_name(new_name)
+        @name = new_name
+    end
+end
+
+doc1 = Document.new("ruby1.txt")
+puts doc1.name
+
+doc1.set_name("ruby2.txt")
+puts doc1.name
