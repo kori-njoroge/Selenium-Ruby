8ca85e0e4f8e7ea60834fa79183dffe8a1fc8feb
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-30 11:24:09 +0300

    Using begin-rescue to check whether a file existing and performs an action of the file if it exists

--- files.rb
+++ files.rb
@@ -9,3 +9,17 @@ aFile.close
 # Reading the conntent of the file
 aFile2 = File.open("Ruby.txt","r")
 aFile2.each{|line| puts line} 
+aFile2.close
+
+# Exception Handling
+def read_my_file(file)
+    begin
+        aFile2 = File.open("#{file}","r")
+        aFile2.each{|line| puts line} 
+        aFile2.close
+    rescue
+        raise("file not Found.  Please make sure the file name is correct")
+    end
+end
+
+read_my_file("arrayes.txt")
