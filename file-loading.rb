ef4559fed9d53e99c22942397bd19e80fccce555
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-30 11:44:33 +0300

    using 'require' to load a file

--- file-loading.rb
+++ file-loading.rb
@@ -5,3 +5,8 @@ load "variable_types.rb"
 file1 = Customer.new(45654, "Gideon","Belmont MA")
 file1.display_info
 
+# # Require - read and parse file only once
+# # require "Ruby.txt" - you can only require ruby files.
+require "/Users/GideonKoriNjoroge/Documents/Kori/Selenium-Ruby/variable_types.rb"
+file1 = Customer.new(45654, "Gideon","Belmont MA")
+file1.display_info
