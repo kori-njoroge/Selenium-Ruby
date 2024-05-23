8b6d4781c4e87d7cf59a9de96f35303aaa189c88
Kori Njoroge <korinjoroge63@gmail.com>
2024-05-23 13:45:27 +0300

    cucumber init

---
+++ Cucumber Project/features/step_definitions/login_steps.rb
@@ -0,0 +1,11 @@
+Given('I open the website in a browser') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I login with credentials {string} and  {string}') do |_string, _string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('home page is displayed') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
